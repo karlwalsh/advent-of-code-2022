7ca84663e538ce024b4fd268c58cd8b1e0b912db
Karl Walsh <karl.walsh@iconsolutions.com>
2022-12-03 18:22:45 +0000

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,56 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun solve(input: List<Int>, n: Int): Int = input.sortedDescending().take(n).sum()
+    fun part1(input: List<String>): Int = solve(input.toDaySpecificInput(), 1)
+    fun part2(input: List<String>): Int = solve(input.toDaySpecificInput(), 3)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    with(::part1) {
+        val exampleResult = this(example())
+        check(exampleResult == 24000) { "Part 1 result was $exampleResult" }
+
+        println("Part 1: ${this(input)}")
+    }
+
+    with(::part2) {
+        val exampleResult = this(example())
+        check(exampleResult == 45000) { "Part 2 result was $exampleResult" }
+
+        println("Part 2: ${this(input)}")
+    }
 }
+
+private fun List<String>.toDaySpecificInput(): List<Int> {
+    val puzzleInput: MutableList<Int> = mutableListOf()
+
+    var currentSum = 0
+    this.forEach { value ->
+        if (value.isBlank()) {
+            puzzleInput.add(currentSum)
+            currentSum = 0
+        } else {
+            currentSum += value.toInt()
+        }
+    }
+
+    if (currentSum > 0) puzzleInput.add(currentSum)
+
+    return puzzleInput
+}
+
+private fun example() = """
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    """.trimIndent().lines()
